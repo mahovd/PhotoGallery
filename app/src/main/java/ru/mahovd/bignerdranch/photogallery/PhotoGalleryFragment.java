449545021852710ee63ead64d507dc1e6d303b9d
Dmitriy Makhov <mahovd@gmail.com>
2016-03-30 22:58:19 +0300

    RecyclerView methods were implemented and connected with AsyncTask

--- app/src/main/java/ru/mahovd/bignerdranch/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/ru/mahovd/bignerdranch/photogallery/PhotoGalleryFragment.java
@@ -9,6 +9,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,6 +23,46 @@ public class PhotoGalleryFragment extends Fragment{
 
     private final static String TAG = "PhotoGalleryFragment";
     private RecyclerView mPhotoRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<>();
+
+    private class PhotoHolder extends RecyclerView.ViewHolder{
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView){
+            super(itemView);
+
+            mTitleTextView = (TextView)itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item){
+            mTitleTextView.setText(item.toString());
+        }
+
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
+
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems){
+            mGalleryItems = galleryItems;
+        }
+
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType){
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        public void onBindViewHolder(PhotoHolder photoHolder, int position){
+            GalleryItem galleryItem = mGalleryItems.get(position);
+            photoHolder.bindGalleryItem(galleryItem);
+        }
+
+        public int getItemCount(){
+            return mGalleryItems.size();
+        }
+
+    }
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -40,20 +84,33 @@ public class PhotoGalleryFragment extends Fragment{
 
         mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
 
-        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        setupAdapter();
 
         return v;
 
     }
 
-    private class FetchItemTask extends AsyncTask<Void,Void,Void>{
-        @Override
-        protected Void doInBackground(Void... params) {
-
-            new FlickrFetch().fetchItems();
-
-            return  null;
+    private void setupAdapter(){
+        if (isAdded()){
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
         }
     }
+
+    private class FetchItemTask extends AsyncTask<Void,Void,List<GalleryItem>>{
+        @Override
+        protected List<GalleryItem> doInBackground(Void... params) {
+
+           return new FlickrFetch().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
+        }
+    }
+
 
 }
